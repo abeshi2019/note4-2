c62a242cf7999f4d933399ab65eeab4387cae4f1
abeshi2019 <56145492+abeshi2019@users.noreply.github.com>
2019-10-07 21:22:50 +0900

    Add files via upload

---
+++ A-18.js
@@ -0,0 +1,14 @@
+var msg;
+var input;
+
+function initial() {
+    msg = document.getElementById('msg');
+    input = document.getElementById('input');
+    msg.textContent = '※本体価格を入力:';
+}
+
+function doclick(e) {
+    var price = input.value * 1;
+    var price2 = price * 1.08;
+    msg.textContent = price + '円の税込価格:' + price2 + '円';
+}
